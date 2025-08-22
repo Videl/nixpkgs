8c71f54d37f518a95351be4a023f6627f16b5ab6
Thibaut Smith <thibaut.smith@mailbox.org>
2025-08-22 14:55:34 +0200

    vk-bootstrap: 0.7 -> 1.4.313
    Diff: https://github.com/charles-lunarg/vk-bootstrap/compare/v0.7...v1.4.313

--- pkgs/by-name/vk/vk-bootstrap/package.nix
+++ pkgs/by-name/vk/vk-bootstrap/package.nix
@@ -5,41 +5,52 @@
   cmake,
   vulkan-headers,
   glfw,
-  catch2,
+  catch2_3,
 }:
 
 stdenv.mkDerivation rec {
   pname = "vk-bootstrap";
-  version = "0.7";
-  outputs = [
-    "out"
-    "dev"
-  ];
+  version = "1.4.313";
 
   src = fetchFromGitHub {
     owner = "charles-lunarg";
     repo = "vk-bootstrap";
     rev = "v${version}";
-    hash = "sha256-X3ANqfplrCF1R494+H5/plcwMH7rbW6zpLA4MZrYaoE=";
+    hash = "sha256-IMqAuJ+zdAaEYdHLAzznz8Cv0ox+tjrgacpfltPWI+M=";
   };
+
+  # sourceRoot = "${src.name}";
+
+  patches = [
+    ./common.patch
+  ];
 
   postPatch = ''
     # Upstream uses cmake FetchContent to resolve glfw and catch2
     # needed for examples and tests
     sed -i 's=add_subdirectory(ext)==g' CMakeLists.txt
-    sed -i 's=Catch2==g' tests/CMakeLists.txt
   '';
 
   nativeBuildInputs = [ cmake ];
   buildInputs = [
     vulkan-headers
     glfw
-    catch2
+    catch2_3
   ];
 
   cmakeFlags = [
     "-DVK_BOOTSTRAP_VULKAN_HEADER_DIR=${vulkan-headers}/include"
+    "-DCatch2_SOURCE_DIR=${catch2_3}/lib/cmake/Catch2/"
   ];
+
+  installPhase = ''
+    mkdir -p $out/{lib,include}
+
+    cp libvk-bootstrap.a $out/lib/
+
+    cp $src/src/VkBootstrap.h $out/include/
+    cp $src/src/VkBootstrapDispatch.h $out/include/
+  '';
 
   meta = with lib; {
     description = "Vulkan Bootstrapping Library";
