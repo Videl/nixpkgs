d0db46b2dd4cbe379d02b3535db8977e31da6973
Pol Dellaiera <pol.dellaiera@protonmail.com>
2026-03-05 16:31:03 +0100

    python3Packages.obsidian-callouts: init at 1.0.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -11277,6 +11277,8 @@ self: super: with self; {
 
   objsize = callPackage ../development/python-modules/objsize { };
 
+  obsidian-callouts = callPackage ../development/python-modules/obsidian-callouts { };
+
   obspy = callPackage ../development/python-modules/obspy { };
 
   obstore = callPackage ../development/python-modules/obstore { };
